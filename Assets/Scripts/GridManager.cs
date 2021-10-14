6eb3948df3d98c224c68685c4b20ae6be6e3d4ea
Archie Adams <archieadams15@gmail.com>
2021-10-14 20:57:38 +0100

    College Mapper
    -Basic grid added
    -Tile object
    -Start of path finding

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField]
+    private int rows = 16;
+    [SerializeField]
+    private int columns = 9;
+    [SerializeField]
+    private int tileSize = 1;
+
+    private readonly List<Tile> tiles = new List<Tile>();
+    
+    public InputField inputField;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    private void GenerateGrid()
+    {
+        GameObject referenceTile = (GameObject)Instantiate(Resources.Load("Tile"));
+        for (int row = 0; row < rows; row++)
+        {
+            for (int column = 0; column < columns; column++)
+            {
+                Tile tile = new Tile(Instantiate(referenceTile, transform), column * tileSize, row * tileSize);
+                tiles.Add(tile);
+                AddNeighbours();
+            }
+        }
+        Destroy(referenceTile);
+
+        float gridWidth = columns * tileSize;
+        float gridHeight = rows * tileSize;
+        transform.position = new Vector3(-(gridWidth- tileSize) / 2 , -(gridHeight-tileSize) / 2);
+    }
+
+    private void AddNeighbours()
+    {
+        foreach (var tile in tiles)
+        {
+            int x = tile.GetX();
+            int y = tile.GetY();
+            for (int xDifference = -1; xDifference <= 1; xDifference++)
+            {
+                for (int yDifference = -1; yDifference <= 1; yDifference++)
+                {
+                    foreach (var neighbourTile in tiles.Where(neighbourTile =>
+                        neighbourTile.GetX() == x + xDifference && neighbourTile.GetY() == y + yDifference && !(xDifference==0 && yDifference == 0)))
+                    {
+                        tile.AddNeighbours(neighbourTile);
+                    }
+                }
+            }
+        }
+    }
+
+    public void SetGoal()
+    {
+        var coordinates = inputField.text;
+        foreach (var tile in tiles.Where(tile => tile.GetCoordinates().Equals(coordinates)))
+        {
+            tile.SetColour(Color.blue);
+            foreach (var neighbourTile in tile.GetNeighbours())
+            {
+                neighbourTile.SetColour(Color.yellow);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    
+    
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile
+{
+
+   private readonly GameObject tile;
+   private int x;
+   private int y;
+
+   private readonly List<Tile> neighbours = new List<Tile>();
+
+
+
+   public Tile(GameObject tile, int x, int y)
+   {
+      this.tile = tile; 
+      this.x = x;
+      this.y = y;
+      this.tile.transform.position = new Vector2(x,y);
+      this.tile.name = "Tile (" + (x+1) + "," + (y+1) + ")";
+   }
+   
+
+   public GameObject GetTile()
+   {
+      return tile;
+   }
+
+   public int GetX()
+   {
+      return x;
+   }
+   
+   public int GetY()
+   {
+      return y;
+   }
+
+   public string GetCoordinates()
+   {
+      return "(" + (x+1) + "," + (y+1) + ")";
+   }
+   
+   public List<Tile> GetNeighbours()
+   {
+      return neighbours;
+   }
+      
+   public void SetColour(Color color)
+   {
+      this.tile.GetComponent<SpriteRenderer>().color = color; 
+   }
+   
+   public void SetX(int x)
+   {
+      this.x = x;
+   }
+   
+   public void SetY(int y)
+   {
+      this.y = y;
+   }
+   
+   public Vector2 GetVector2()
+   {
+      return tile.transform.position;
+   }
+   
+   public void SetVector2(int x, int y)
+   {
+      this.tile.transform.position = new Vector2(x,y);
+   }
+
+   public void AddNeighbours(Tile tile)
+   {
+      neighbours.Add(tile);
+   }
+}
+   
+
+
